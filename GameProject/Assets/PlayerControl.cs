1f0387c88229662bcc9eb5d9e51f85dbd8033ec1
woody8876 <bon8876@gmail.com>
2018-08-15 14:21:52 +0800

    PlayerControl

---
+++ GameProject/Assets/PlayerControl.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour {
+
+    private float speed=10f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float playerMove=Input.GetAxis("Horizontal");
+
+        transform.position += transform.right * playerMove * speed*Time.deltaTime;
+
+	}
+}
