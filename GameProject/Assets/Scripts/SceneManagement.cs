a1589a16ce591fa1cfdd94e28ab1459a9218e914
bryan75324 <37688239+bryan75324@users.noreply.github.com>
2018-08-15 13:37:04 +0800

    Open game project

---
+++ GameProject/Assets/Scripts/SceneManagement.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneManagement : MonoBehaviour
+{
+    private static SceneManagement m_Instance;
+    public static SceneManagement Instance { get { return m_Instance; } }
+
+    private void Awake()
+    {
+        if (m_Instance == null)
+            m_Instance = this;
+        else if (m_Instance != this)
+            Destroy(this.gameObject);
+    }
+
+    // Use this for initialization
+    private void Start()
+    {
+        DontDestroyOnLoad(this.gameObject);
+    }
+
+    public void LoadNextScene()
+    {
+    }
+}
