bb4207efc3588de704f12c481dd4565bb1b1cda3
woody8876 <bon8876@gmail.com>
2018-08-15 16:26:36 +0800

    PlayerData

--- GameProject/Assets/PlayerControl.cs
+++ GameProject/Assets/PlayerControl.cs
@@ -7,7 +7,9 @@ public class PlayerControl : MonoBehaviour
 {
 
     private float m_speedX = 3f;
-    private float m_speedY;
+    private float m_speedLeft = 3f;
+    private float m_speedY = 150f;
+    private float playerMove;
     private Rigidbody2D m_Rigidbody2D;
     // Use this for initialization
     void Start()
@@ -18,19 +20,34 @@ public class PlayerControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        float playerMove = Input.GetAxis("Horizontal");
+        playerMove = Input.GetAxis("Horizontal");
         transform.position += transform.right * playerMove * m_speedX * Time.deltaTime;
     }
     private void FixedUpdate()
     {
         if (Input.GetKeyDown(KeyCode.Space))
         {
-            m_Rigidbody2D.AddForce(Vector2.up * 150f);
+            m_Rigidbody2D.AddForce(Vector2.up * m_speedY);
         }
         if (Input.GetKey(KeyCode.LeftControl))
         {
-            m_speedY = -1.0f;
-            m_Rigidbody2D.velocity = new Vector2(0f,m_speedY);
+            m_Rigidbody2D.velocity = new Vector2(0f, -1.0f);
         }
+    }
+    /// <summary>
+    /// 水平速度變化，參數傳遞-2~2
+    /// </summary>
+    /// <param name="speedX"></param>
+    public void SpeedVarietyX(int speedX)
+    {
+        m_Rigidbody2D.velocity = new Vector2(speedX, 0f);
+    }
+    /// <summary>
+    /// 往上跳躍
+    /// </summary>
+    /// <param name="speedY"></param>
+    public void SpeedVarietyY(int speedY)
+    {
+        m_Rigidbody2D.AddForce(Vector2.up * speedY);
     }
 }
---
+++ GameProject/Assets/PlayerData.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerData : MonoBehaviour {
+    PlayerControl player=new PlayerControl;
+    public int playerHP = 10;
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void BeingAttacked(int damage)
+    {
+        playerHP -= damage;
+    }
+}
