0b2325543341453b15ff887f118c53e7fa36be05
woody8876 <bon8876@gmail.com>
2018-08-15 15:26:46 +0800

    PlayerControl

--- GameProject/Assets/PlayerControl.cs
+++ GameProject/Assets/PlayerControl.cs
@@ -1,20 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class PlayerControl : MonoBehaviour {
+public class PlayerControl : MonoBehaviour
+{
 
-    private float speed=10f;
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-        float playerMove=Input.GetAxis("Horizontal");
+    private float m_speedX = 3f;
+    private float m_speedY;
+    private Rigidbody2D m_Rigidbody2D;
+    // Use this for initialization
+    void Start()
+    {
+        m_Rigidbody2D = this.GetComponent<Rigidbody2D>();
+    }
 
-        transform.position += transform.right * playerMove * speed*Time.deltaTime;
-
-	}
+    // Update is called once per frame
+    void Update()
+    {
+        float playerMove = Input.GetAxis("Horizontal");
+        transform.position += transform.right * playerMove * m_speedX * Time.deltaTime;
+    }
+    private void FixedUpdate()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            m_Rigidbody2D.AddForce(Vector2.up * 150f);
+        }
+        if (Input.GetKey(KeyCode.LeftControl))
+        {
+            m_speedY = -1.0f;
+            m_Rigidbody2D.velocity = new Vector2(0f,m_speedY);
+        }
+    }
 }
